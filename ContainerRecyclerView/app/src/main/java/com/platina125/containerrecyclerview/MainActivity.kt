0441a0009f8074086c065025136412af22223cde
bexter125 <bexter125@naver.com>
2022-10-19 12:08:31 +0900

    myKotlin

--- ContainerRecyclerView/app/src/main/java/com/platina125/containerrecyclerview/MainActivity.kt
+++ ContainerRecyclerView/app/src/main/java/com/platina125/containerrecyclerview/MainActivity.kt
@@ -2,18 +2,33 @@ package com.platina125.containerrecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.platina125.containerrecyclerview.databinding.ActivityMainBinding
+import java.util.zip.Inflater
 
 class MainActivity : AppCompatActivity() {
+    val binding by lazy{ActivityMainBinding.inflate(layoutInflater)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
-        // 100개의 list를 가상으로 만듭니다.
-        fun loadData(): MutableList<Memo>{
-            val data: MutableList<Memo> = mutableListOf()
-            for (no in 1..100){
+        val data:MutableList<Memo> = loadData()
+        var adapter = CustomAdapter()
+        adapter.listData = data
+        binding.recyclerView.adapter = adapter
 
-            }
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
+    // 100개의 list를 가상으로 만듭니다.
+    fun loadData(): MutableList<Memo>{
+        val data: MutableList<Memo> = mutableListOf()
+        for (no in 1..100){
+            val title = "이것이 안드로이드다 ${no}"
+            val date = System.currentTimeMillis()
+            var memo = Memo(no, title, date)
+            data.add(memo)
         }
+        return data
     }
 }
