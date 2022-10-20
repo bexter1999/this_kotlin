1c4ba9b8cfc9632dd258c82a3bbc3390bdb49c61
bexter125 <bexter125@naver.com>
2022-10-20 12:25:46 +0900

    myKotlin

--- Fragment/app/src/main/java/com/platina125/fragment/ListFragment.kt
+++ Fragment/app/src/main/java/com/platina125/fragment/ListFragment.kt
@@ -11,18 +11,23 @@ import com.platina125.fragment.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
     var mainActivity: MainActivity? = null
+    lateinit var binding:FragmentListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        /*return inflater.inflate(R.layout.fragment_list, container, false)*/
-        val binding = FragmentListBinding.inflate(inflater, container, false)
+
         binding.btnNext.setOnClickListener { mainActivity?.goDetail() }
+
+        binding.textTitle.text = arguments?.getString("key1")
+        binding.textValue.text = "${arguments?.getInt("key2")}"
         return binding.root
     }
 
+    fun setValue(value: String){
+        binding.textFromActivity.text = value
+    }
     override fun onAttach(context: Context) {
         super.onAttach(context)
 
--- Fragment/app/src/main/java/com/platina125/fragment/MainActivity.kt
+++ Fragment/app/src/main/java/com/platina125/fragment/MainActivity.kt
@@ -2,16 +2,28 @@ package com.platina125.fragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.platina125.fragment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    val binding by lazy{ActivityMainBinding.inflate(layoutInflater)}
+    lateinit var listFragment:ListFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
         setFragment()
+        
+        binding.btnSend.setOnClickListener { 
+            listFragment.setValue("전달할 값")
+        }
     }
     fun setFragment(){ //리스트 Fragment 생성
-        val listFragment: ListFragment = ListFragment()
+
+        //프래그먼트 생성시 값 전달하기 위한 변수설정 부분 3줄
+        val bundle = Bundle()
+        bundle.putString("key1","List Fragment")
+        bundle.putInt("key2",20220101)
+        listFragment.arguments = bundle
 
         val transaction = supportFragmentManager.beginTransaction()
         // 트랜잭션의 add() 메서드로 frameLayout을 id로 가지고 있는 레이아ㅜㅅ에 앞에서 생성한 listFragment 삽입
