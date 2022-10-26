23a80200264500f3f4c9c8ed4219183ef96bb9b0
bexter125 <bexter125@naver.com>
2022-10-26 17:51:18 +0900

    myKotlin

---
+++ ProjectMobile/app/src/main/java/com/platina125/projectmobile/Health.kt
@@ -0,0 +1,11 @@
+package com.platina125.projectmobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Health : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_health)
+    }
+}
