b85f0026a86557d792c8f30a9ff94f926a2c67bb
bexter125 <bexter125@naver.com>
2022-10-27 11:44:57 +0900

    memo insert delete

--- SQLite/app/src/main/java/com/platina125/sqlite/MainActivity.kt
+++ SQLite/app/src/main/java/com/platina125/sqlite/MainActivity.kt
@@ -2,10 +2,36 @@ package com.platina125.sqlite
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.platina125.sqlite.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    val helper = SqliteHelper(this, "memo", 1)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val adapter = RecyclerAdapter()
+        adapter.helper = helper
+        //adapter의 listData에 데이터베이스에서 가져온 데이터를 세팅
+        adapter.listData.addAll(helper.selectMemo())
+
+        //화면의 리사이클러뷰 위젯에 adapter를 연결하고 레이아웃 매니저를 설정
+        binding.recyclerMemo.adapter = adapter
+        binding.recyclerMemo.layoutManager = LinearLayoutManager(this)
+
+        binding.buttonSave.setOnClickListener {
+            if (binding.editMemo.text.toString().isNotEmpty()) {
+                val memo = Memo(null, binding.editMemo.text.toString(), System.currentTimeMillis())
+
+                helper.insertMemo(memo)
+                adapter.listData.clear()
+                adapter.listData.addAll(helper.selectMemo())
+                adapter.notifyDataSetChanged()
+                binding.editMemo.setText("")
+            }
+        }
     }
 }
